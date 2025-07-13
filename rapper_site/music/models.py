c7b0e833f194051095782f9bd89d0203a45080e6
Timur Pustoplesnov <timur.pustoplesnov@yandex.ru>
2025-07-13 16:40:00 +0300

    2025-07-13  add merch app and update models.py

--- rapper_site/music/models.py
+++ rapper_site/music/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    release_date = models.DateField()
+    cover = models.ImageField(upload_to='albums/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Track(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    duration = models.DurationField()
+    audio_file = models.FileField(upload_to='tracks/')
+    youtube_link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.album})"
+
